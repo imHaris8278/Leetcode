53d658a82db305ebec60c3db8ea3e8fbeec4b7a9
Mirza Muhammad Haris Baig <harisbaig7081@gmail.com>
2025-03-05 05:45:10 +0500

    Time: 2 ms (25.62%) | Memory: 7.8 MB (61.42%) - LeetSync

---
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    long long coloredCells(int n) {
+        long long numBlueCells = 1;
+        int addend = 4;
+
+        while (--n)
+        {
+            numBlueCells += addend;
+            addend += 4;
+        }
+        
+        return numBlueCells;
+    }
+};
