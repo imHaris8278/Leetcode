ccdb8a9b50ad9ad962b403cfd263895c1aa96880
Mirza Muhammad Haris Baig <harisbaig7081@gmail.com>
2025-10-14 09:58:50 +0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (35.93%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countWays(int n, vector<int>& dp) {
+        if(n == 1)
+        {
+            return 1;
+        }
+
+        if(n == 2)
+        {
+            return 2;
+        }
+
+        if(dp[n] != -1)
+        {
+            return dp[n];
+        }
+
+        dp[n] = countWays(n - 1, dp) + countWays(n - 2, dp);
+        return dp[n];
+    }
+
+    int climbStairs(int n) 
+    {
+        vector<int> dp(n + 1, -1);
+
+        return countWays(n, dp);
+    }
+};
